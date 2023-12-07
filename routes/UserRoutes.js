2d40da6b60401e9c0cb6df0a7ece01e52d6ed9d5
Abderrahmen Malouche <abderrahmenmalouche12@gmail.com>
2023-12-08 00:28:46 +0100

    finished

--- routes/UserRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-const router = express.Router();
-import { registerUser,loginUser } from "../controllers/userControllers";
-
-router.post('/signup',registerUser)
-router.post('/login',loginUser)
-
-
-export default router;
